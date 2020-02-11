dd2c64c9c0be2ce77cb50c376c29799ec34b2524
olcplif <oleksandr.lysenko.jr@gmail.com>
2020-02-11 22:35:41 +0200

    Add def hello and goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡Hello, Soft Serve!"
+  end
+
+  def goodbye
+    render html: "Goodbye, user!"
+  end
+
 end
